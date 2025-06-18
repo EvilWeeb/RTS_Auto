dac91af5a4803e8fff91486de43007375bd7478e
helesta <alexwyz233@hotmail.com>
2025-06-18 18:11:17 +0800

    三点隐藏菜单按钮选择逻辑优化

--- tasks/pull_history_acc.py
+++ tasks/pull_history_acc.py
@@ -10,10 +10,15 @@ def pull_history_acc(page, row:dict):
     page.get_by_role("menuitem", name="餐厅列表").click()
     page.get_by_placeholder("请输入餐厅号/餐厅名").fill(store_code)
     page.get_by_role("button", name="查询", exact=True).click()
-    # page.locator("span.anticon.font-icon[role='img']").hover()
-    page.locator(f'tr:has(td:text-is("{store_code}")) span.anticon.font-icon[role="img"]').hover()
-    page.wait_for_selector("text=账号管理", timeout=3000)
-    page.get_by_text("账号管理").click()
+    page.locator(f'//tr[td[normalize-space(text())="{store_code}"]]//span[@class="anticon font-icon" and @role="img"]').hover()
+    # 查找 store_code 在第几行（从 2 开始，td[2] 是编号列）
+    for i in range(2, 52):
+        if page.locator(f'xpath=//*[@id="container"]/div/div/div/div/div[2]/div[2]/div/div/div/div/div/div/table/tbody/tr[{i}]/td[2]').inner_text() == store_code:
+            index = i - 2
+            break
+    # 构造 XPath 点击账号管理
+    page.wait_for_selector(f'xpath=//*[@id="root-web-boss"]/div[{index + 7}]/div/div/ul/li[4]/span/a[text()="账号管理"]', timeout=3000)
+    page.locator(f'xpath=//*[@id="root-web-boss"]/div[{index + 7}]/div/div/ul/li[4]/span/a[text()="账号管理"]').click()
     page.get_by_role("button", name="拉取历史账号").click()
 
 
--- tasks/set_DBM_Merchantid.py
+++ tasks/set_DBM_Merchantid.py
@@ -12,9 +12,15 @@ def set_DBM_Merchantid(page, row:dict):
     page.get_by_role("menuitem", name="餐厅列表").click()
     page.get_by_placeholder("请输入餐厅号/餐厅名").fill(uscode)
     page.get_by_role("button", name="查询", exact=True).click()
-    page.locator(f'tr:has(td:text-is("{uscode}")) span.anticon.font-icon[role="img"]').hover()
-    page.wait_for_selector("text=DMB及商户号设置", timeout=3000)
-    page.get_by_text("DMB及商户号设置").last.click()
+    page.locator(f'//tr[td[normalize-space(text())="{uscode}"]]//span[@class="anticon font-icon" and @role="img"]').hover()
+    # 查找 store_code 在第几行（从 2 开始，td[2] 是编号列）
+    for i in range(2, 52):
+        if page.locator(f'xpath=//*[@id="container"]/div/div/div/div/div[2]/div[2]/div/div/div/div/div/div/table/tbody/tr[{i}]/td[2]').inner_text() == uscode:
+            index = i - 2
+            break
+    # 构造 XPath 点击账号管理
+    page.wait_for_selector(f'xpath=//*[@id="root-web-boss"]/div[{index + 7}]/div/div/ul/li[4]/span/a[text()="DMB及商户号设置"]', timeout=3000)
+    page.locator(f'xpath=//*[@id="root-web-boss"]/div[{index + 7}]/div/div/ul/li[4]/span/a[text()="DMB及商户号设置"]').click()
     page.locator("#list_0_unionPayDealerCode").fill(Merchantid)
     if dbmip != "nan":  # 只有不是 NaN 时才执行填充
         page.locator("#list_0_dmbDeviceIp").fill(dbmip)
